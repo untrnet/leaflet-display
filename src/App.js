431c5d04f27947c363fe45e4c07a020d5b011110
tbscanlon <thomas.scanlon@outlook.com>
2018-10-09 13:32:59 +0100

    Did a thing

--- src/App.js
+++ src/App.js
@@ -3,24 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isInverted: false
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ isInverted: false });
+    }, 2000);
+
+    setInterval(() => {
+      this.setState({ isInverted: true });
+    }, 6500);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <div className={this.state.isInverted ? "App-invert" : "App"}>
+      <div>
+        <h1>TAKE A LEAFLET</h1>
       </div>
+    </div>
     );
   }
 }
